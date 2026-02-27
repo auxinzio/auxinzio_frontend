6c2b12c23769dd3f0a1d9fbc06dce35e3b136045
B.Santhoshkumar <bsanthoshkumar10@gmail.com>
2026-02-27 13:45:01 +0530

    logs added

--- src/app/Context/AuthContext.jsx
+++ src/app/Context/AuthContext.jsx
@@ -68,13 +68,7 @@ export function AuthProvider({ children }) {
 
       const data = await res.json();
 
-      console.log(data);
-      console.log(res);
-      console.log(res.ok);
-      console.log(data.success);
-      console.log(res.ok && data.success);
-
-      if (res.ok && data.success) {
+      if (res.ok && data.statuscode == 200) {
         const userData = data.data;
         setUser(userData.user || userData);
         router.push('/admin/dashboard');
